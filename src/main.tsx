32188326a29e8a8d4b9205ae8e092f715bd29fd1
oxamyt <dentez.danil@gmail.com>
2024-12-05 18:08:28 +0200

    setup project and dependencies

---
+++ src/main.tsx
@@ -0,0 +1,12 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
+import router from "./routes.jsx";
+import "./index.css";
+
+const root = createRoot(document.getElementById("root")!);
+root.render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
+);
